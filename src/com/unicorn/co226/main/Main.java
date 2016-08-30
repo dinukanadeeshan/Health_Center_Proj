9b30e090e21129a7a676c56fc1d6e9c7f32e94d3
dinukanadeeshan <dinuka.nadeeshan@yahoo.com>
2016-08-30 14:05:16 +0530

    Initial Commit

---
+++ src/com/unicorn/co226/main/Main.java
@@ -0,0 +1,12 @@
+package com.unicorn.co226.main;
+
+/**
+ * Project - HealthCenterProj
+ * Created by Dinuka Nadeeshan on 8/30/16.
+ * dinuka.nadeeshan1993@gmail.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Initial State...");
+    }
+}
